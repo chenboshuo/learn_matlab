5433bbde0a17f4225b6d3b5908f856960646084f
yichuanshuzi <chenboshuo.hi@163.com>
2019-06-22 10:30:14 +0800

    分段函数 突然发现可以不设置变量

--- basic/s_function.m
+++ basic/s_function.m
@@ -1,9 +1,28 @@
-function y = s_function(x)
+function s_function(x)
   if x>1
-    y = x^2 + 1
+    x^2 + 1
   else if x>0
-    y = 2*x
+    2*x
   else
     x^3
   end
 end
+
+% >> s_function(-2)
+%
+% ans =
+%
+%     -8
+%
+% >> s_function(0.5)
+%
+% ans =
+%
+%      1
+%
+%
+% >> s_function(5)
+%
+% ans =
+%
+%     26
